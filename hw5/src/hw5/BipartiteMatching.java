11ba5507b4acfe978f81d3af77b085d6c112ded8
chilliamli <william9@seas.upenn.edu>
2021-04-22 16:58:00 -0400

    COMMIT PLZ

--- hw5/src/hw5/BipartiteMatching.java
+++ hw5/src/hw5/BipartiteMatching.java
@@ -1,5 +1,12 @@
 package hw5;
 
 public class BipartiteMatching {
-
+    //Implement Matchin scheme with FF
+    
+    //Modify graph
+    
+    //Run FF algorihtm: 
+    public void FordFulkerson(Graph g) {
+        
+    }
 }
--- hw5/src/hw5/ClosureProperties.java
+++ hw5/src/hw5/ClosureProperties.java
@@ -1,5 +1,38 @@
 package hw5;
 
 public class ClosureProperties {
+    
+//All graph properties can be found in graph.java
 
+    // Inputs our matched graph, and calculates Homophily 
+    public float homophily (Graph g) {
+        return 0; //TODO
+    }
+    //Calculates clustering coefficient
+    public float clusteringCoefficients (Graph g) {
+        return 0; //TODO
+    }
+    //Checks if focal closure is satisfied
+    public boolean focalClosure (Graph g) {
+        return true; //TODO
+    }
+    //Checks if membership closure is satisfied
+    public boolean membershipClosure (Graph g) {
+        return true; //TODO
+    }
+    //Checks if triadic closure is satisfied
+    public boolean triadicClosure (Graph g) {
+        return true; //TODO
+    }
+    //Checks to see if our tutor/student graph is a bipartite graph
+    public boolean BipartiteMatching (Graph g) {
+        return true; //TODO
+    }
+    
+    //Calculates the number of subtrees and finds a numerical representation for how connected
+    //our graph is (basically how interconnected our tutor/student group is)
+    public boolean DFSConnectedness (Graph g) {
+        return true; //TODO
+    }
+    
 }
