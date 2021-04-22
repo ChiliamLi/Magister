821d5d0392ac4966e9f95e8f6f048aecb251d760
Jeffrey Qu <jqu@seas.upenn.edu>
2021-04-22 16:23:49 -0400

    Initial commit

--- hw5/src/hw5/Data.java
+++ hw5/src/hw5/Data.java
@@ -3,7 +3,12 @@ import org.json.simple.*;
 
 
 public class Data {
-    JSONObject student = new JSONObject();
     
+    public JSONObject createEmployee() {
+        JSONObject student = new JSONObject();
+        student.put("id", 0);
+        student.put("name", "William Li");s
+        return null;
+    }
     
 }
